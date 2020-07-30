c3b81cf190f274fb69cdb723815a01c7a6c13947
MarileneGarcia <marilenegarcia1998@gmail.com>
2020-07-30 17:11:33 -0300

    versao_final

--- questao2.py
+++ questao2.py
@@ -1,12 +1,16 @@
-# Baseado nos codigos vistos em aula
-# Tem que instalar ums bibliotecas: 
-    # python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+# Baseado nos codigos apresentados em aula pelo professor
+# Deve-se instalar algumas bibliotecas: 
+    # python -m pip install --user numpy scipy matplotlib sympy 
 
 # Escolher o valor de k
 k = 100
 
-# Escolhar as condicoes d
+# Escolhar as condicoes:
+# Restricao natural (comente a linha 68)
 cond = 'natural'
+# Derivada conhecida nos extremos (remova o comentario da linha 68)
+A = 2.5
+B = 2.5
 
 import numpy as np
 import sympy as sym
@@ -59,6 +63,9 @@ def spline( ):
 
     # Aplicando o metodo spline interpolante usando a restricao natural
     cs = sci.CubicSpline(x_nodes, y_nodes, bc_type = ( cond ))
+
+    # Aplicando o metodo spline interpolante usando a restricao da derivada conhecida nos extremos
+    #cs = sci.CubicSpline(x_nodes, y_nodes, bc_type = ( (1, A), (1, B) ) )
 
     # Calcular o erro atual
     cs_x_eval = cs( x_eval )
